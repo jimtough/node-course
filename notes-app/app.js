d1652b7ac770b4830f9318e5950e49cd70d6e79d
adultmalehuman <adultmalehuman@gmail.com>
2020-01-20 15:49:14 -0400

    After section on printing to the console in color

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,9 @@
-const validator = require('validator')
 const getNotes = require('./notes.js')
+const chalk = require('chalk')
 
 const msg = getNotes()
 
 console.log(msg)
-
-console.log(validator.isURL('https://mead.io'))
+console.log(chalk.green('Success!'))
+console.log(chalk.white.bgGreen.bold('Great Success!!'))
+console.log(chalk.green.inverse('green inverse'))
