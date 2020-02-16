4cecabf37a83a642c9859dceba417db5545e0742
adultmalehuman <adultmalehuman@gmail.com>
2020-02-16 11:04:12 -0400

    Basic handling of error when using 'request' to make HTTP call

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,18 +4,20 @@ const request = require('request')
 // REFERENCE: https://darksky.net/dev/docs#forecast-request
 const darkskyUrl = 'https://api.darksky.net/forecast/ead7bc2e808f9a216d9f391538d1ede7/44.7247,-63.6909?key=value&otherkey=othervalue&units=ca'
 
-/*
-COMMENTING OUT WHILE EXPERIMENTING WITH MAPBOX API BELOW...
 // Use 'request' module to request weather data from the DarkSky API.
 // By passing the 'json: true' parameter in its options object, we should get back a JSON-formatted response.
 request({ url: darkskyUrl, json: true }, (error, response)=>{
-    //console.log(response.body.currently)
-    console.log("It is currently " + response.body.currently.temperature + 
-        " C. There is a " + response.body.currently.precipProbability + 
-        "% chance of rain. Daily summary: '" + response.body.daily.data[0].summary + "'")
+    if (error) {
+        //console.log(error)
+        console.log('Unable to connect to weather service')
+    } else {
+        console.log("It is currently " + response.body.currently.temperature + 
+            " C. There is a " + (parseFloat(response.body.currently.precipProbability) * 100) + 
+            "% chance of rain. Daily summary: '" + response.body.daily.data[0].summary + "'")
+    }
 })
-*/
 
+/*
 // REFERENCE: https://docs.mapbox.com/api/search/
 const myMapboxAccessToken = 'pk.eyJ1IjoiYWR1bHRtYWxlaHVtYW4iLCJhIjoiY2s2cDNqYXFlMWk3YjNocXR3Y2NmMmhrYiJ9.jGwnHAeVK98giUOPZHFbfg'
 //const mapboxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?limit=1&language=en-CA&access_token=pk.eyJ1IjoiYWR1bHRtYWxlaHVtYW4iLCJhIjoiY2s2cDNqYXFlMWk3YjNocXR3Y2NmMmhrYiJ9.jGwnHAeVK98giUOPZHFbfg'
@@ -31,4 +33,4 @@ request({ url: mapboxUrl, json: true }, (error, response)=>{
     const longitude = response.body.features[0].center[0]
     console.log("The lat/lon of '" + matchingPlaceName + "' is " + latitude + ', ' + longitude + '.')
 })
-
+*/
